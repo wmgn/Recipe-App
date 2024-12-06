692465db3d67922349b7e8e70f536896cd29212c
aryan <aryananwar221@gmail.com>
2024-12-06 17:07:58 -0500

    a
    a

--- app/src/main/java/com/example/recipeapp/Favorites.kt
+++ app/src/main/java/com/example/recipeapp/Favorites.kt
@@ -3,10 +3,10 @@ package com.example.recipeapp
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class AddRecipe: AppCompatActivity() {
+class Favorites: AppCompatActivity() {
      fun onCreate() {
         enableEdgeToEdge()
-         setContentView(R.layout.add_recipe)
+         setContentView(R.layout.favorite_recipes)
 
      }
 }
